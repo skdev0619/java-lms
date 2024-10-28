cab9d6c314d8be477dc96dad0befedbf61e1dba2
skdev0619 <skdev0619@naver.com>
2024-10-28 14:26:03 +0900

    매직넘버 제거 및 유효성 체크 변경

--- src/main/java/nextstep/courses/domain/Size.java
+++ src/main/java/nextstep/courses/domain/Size.java
@@ -1,41 +1,43 @@
 package nextstep.courses.domain;
 
-import javax.swing.*;
-
 public class Size {
 
-    private static final int DEFAULT_WIDTH = 300;
-    private static final int DEFAULT_HEIGHT = 200;
+    private static final double WIDTH_RADIO = 3;
+    private static final double HEIGHT_RADIO = 2;
+
+    private static final int MIN_WIDTH = 300;
+    private static final int MIN_HEIGHT = 200;
 
     private final int width;
     private final int height;
 
     public Size(int width, int height) {
+        validate(width, height);
         this.width = width;
         this.height = height;
-        validate();
     }
 
-    private void validate() {
-        validateSize();
-        validateRatio();
+    private void validate(int width, int height) {
+        validateSize(width, height);
+        validateRatio(width, height);
     }
 
-    private void validateSize() {
-        if(width < DEFAULT_WIDTH && height < DEFAULT_HEIGHT){
+    private void validateSize(int width, int height) {
+        if (width < MIN_WIDTH || height < MIN_HEIGHT) {
             throw new IllegalArgumentException(
                     String.format("가로 %d픽셀, 세로 %d픽셀 이상이어야 합니다",
-                            DEFAULT_WIDTH, DEFAULT_HEIGHT)
+                            MIN_WIDTH, MIN_HEIGHT)
             );
         }
     }
 
-    private void validateRatio() {
+    private void validateRatio(int width, int height) {
         double ratio = (double) width / height;
-        double expectedRatio = 3.0 / 2.0;
+        double expectedRatio = WIDTH_RADIO / HEIGHT_RADIO;
 
-        if(ratio != expectedRatio){
-            throw new IllegalArgumentException("가로와 세로의 비율은 3:2여야 합니다");
+        if (ratio != expectedRatio) {
+            throw new IllegalArgumentException(
+                    String.format("가로와 세로의 비율은 %d:%d여야 합니다", WIDTH_RADIO, HEIGHT_RADIO));
         }
     }
 }
